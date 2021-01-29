26b9d11fb382f9640c31662425d20208933c99a7
as <>
2021-01-29 19:11:22 +0900

    カプセル化

---
+++ C-Object/C-Object/Money.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace C_Object
+{
+    public class Money
+    {
+        public Money(decimal value)
+        {
+            Value = Value;
+        }
+
+        public decimal Value { get; }
+
+        public decimal TaxValue
+        {
+            get
+            {
+                return Value * 1.08m;
+            }
+        }
+    }
+}
--- C-Object/C-Object/Program.cs
+++ C-Object/C-Object/Program.cs
@@ -4,9 +4,21 @@ namespace C_Object
 {
     class Program
     {
-        static void Main(string[] args)
+        private void ShowButton_Click()
         {
-            Console.WriteLine("Hello World!");
+            var val = GetAmount();
+            ShowLabel.Text = (val * 1.08m).ToString() + "円";
+        }
+
+        private decimal GetAmount()
+        {
+            return 100m;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var money = new Money(GetAmount());
+            ShowLabel.Text = money.TaxValue + "円";
         }
     }
 }
